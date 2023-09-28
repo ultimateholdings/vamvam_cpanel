a9e3e50ed3e79573df1d6239f1dd7edb4dbffeae
parfait ebobisse epoune <parfaitebobisse@gmail.com>
2023-09-28 14:21:16 +0200

    deploy organisation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://ultimateholdings.github.io/vamvam_panel"
-  base:"/"
+  base: "https://ultimateholdings.github.io/vamvam_panel"
+  // base:"/"
 })
