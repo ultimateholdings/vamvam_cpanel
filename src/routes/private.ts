9bdf0229d43f2da2c849a9ec8f260eba73745b1a
Christine mirhyne Nyembe <nyembe.mirhyne@gmail.com>
2023-10-11 12:30:41 +0100

    form without validate

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 
 const Profile = lazy(() => import('../pages/Profile'));
 const UserList = lazy(() => import('../modules/usersModule/pages/UserList'));
+const UserCreate = lazy(() => import('../modules/usersModule/pages/UserCreate'));
 
 /* mettre les routes protégées ici */
 const coreRoutes = [
@@ -14,6 +15,11 @@ const coreRoutes = [
         path: '/users',
         title: 'Users - List',
         component: UserList,
+    },
+    {
+        path: '/user-create',
+        title: 'Create new User',
+        component: UserCreate,
     },
 ];
 
