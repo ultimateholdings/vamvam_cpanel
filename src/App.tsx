42f9f8b380a1f39620843fd97901e50555aa6e1d
Ndimah Mike <ndimah22@protonmail.com>
2024-03-11 15:52:23 +0100

    feat(UI): update the login UI with chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
+import { ChakraProvider, extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 import { router } from './routes';
 import '../i18n';
 import i18n from '../i18n';
-import { useEffect } from 'react';
+import { useEffect, StrictMode } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { STORAGE_KEY } from './helper/enums';
 import useThemeMode from './hooks/useThemeMode';
@@ -13,41 +14,56 @@ import { AppDispatch, RootState } from './store';
 import CircularLoader from './components/UI/CircularLoader';
 import { getAuthToken } from './helper/utils';
 
+const customTheme = extendTheme(
+    {
+        colors: {
+            brand: {
+                100: '#3C50E0'
+            }
+        }
+    },
+    withDefaultColorScheme({
+        colorScheme: "blue",
+        components: ["Button", "Badge"]
+    })
+);
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-  const { error, loading, userData } = useSelector(
-    (state: RootState) => state.profile,
-  );
+    const dispatch = useDispatch<AppDispatch>();
+    const { error, loading, userData } = useSelector(
+        (state: RootState) => state.profile,
+    );
 
-  useThemeMode();
+    useThemeMode();
 
-  useEffect(() => {
-    const lng = localStorage.getItem(STORAGE_KEY.lang) ?? navigator.language;
-    i18n.changeLanguage(lng);
-  }, []);
+    useEffect(() => {
+        const lng = localStorage.getItem(STORAGE_KEY.lang) ?? navigator.language;
+        i18n.changeLanguage(lng);
+    }, []);
 
-  useEffect(() => {
-    if (!userData && getAuthToken()) {
-      dispatch(fetchUserData());
+    useEffect(() => {
+        if (!userData && getAuthToken()) {
+            dispatch(fetchUserData());
+        }
+    }, [dispatch, userData]);
+
+    let mainContent = <RouterProvider router={router} />;
+
+    if (loading) {
+        mainContent = <CircularLoader />;
     }
-  }, [dispatch, userData]);
 
-  let mainContent = <RouterProvider router={router} />;
+    if (error) {
+        mainContent = <div>{error.message}</div>;
+    }
 
-  if (loading) {
-    mainContent = <CircularLoader />;
-  }
-
-  if (error) {
-    mainContent = <div>{error.message}</div>;
-  }
-
-  return (
-    <>
-      <Toast />
-      {mainContent}
-    </>
-  );
+    return (
+        <StrictMode>
+            <ChakraProvider theme={customTheme}>
+                <Toast />
+                {mainContent}
+            </ChakraProvider>
+        </StrictMode>
+    );
 }
 
 export default App;
---
+++ src/components/UI/index.tsx
@@ -0,0 +1,30 @@
+import {Logo} from "./Logo.tsx"
+import CircularLoader from "./CircularLoader.tsx";
+import DarkModeSwitcher from "./DarkModeSwitcher.tsx";
+import Toast from "./Toast.tsx";
+import  * as Icons from "./ProviderIcons.tsx";
+import DropdownMessage from "./DropdownMessage.tsx";
+import DropdownNotification from "./DropdownNotification.tsx";
+import DropdownUser from "./DropdownUser.tsx";
+import Header from "./Header.tsx";
+import Layout from "./Layout.tsx";
+import LinearLoader from "./LinearLoader.tsx";
+import {OAuthButtonGroup} from "./OAuthButtonGroup.tsx";
+import {PasswordField} from "./PasswordField.tsx";
+import SidebarLinkGroup from "./SidebarLinkGroup.tsx";
+import Sidebar from "./Sidebar.tsx";
+
+export {
+    CircularLoader,
+    DarkModeSwitcher,
+    DropdownNotification,
+    DropdownUser,
+    Icons,
+    Layout,
+    LinearLoader,
+    Logo,
+    OAuthButtonGroup,
+    PasswordField,
+    Sidebar,
+    SidebarLinkGroup
+}
